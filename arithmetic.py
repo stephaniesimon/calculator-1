ac859237371cb64a91bc9785c8d9f77f0c9acbab
Hackbright Student <info@hackbrightacademy.com>
2015-07-01 15:51:18 -0700

    first commit

---
+++ arithmetic.py
@@ -0,0 +1,32 @@
+def add(num1, num2):
+    print num1 + num2
+
+def subtract(num1, num2):
+    print num1 - num2
+
+def multiply(num1, num2):
+    print num1 + num2
+
+def divide(num1, num2):
+    print num1 / num2
+
+def square(num):
+    print num * num
+
+def cube(num):
+    print num * num * num
+
+def power(num1, num2):
+    print num1 ** num2
+
+def mod(num1, num2):
+    print num1 % num2
+
+add(6,5)
+subtract(6,5)
+multiply(6,5)
+divide(6,5)
+square(6)
+cube(6)
+power(6,5)
+mod(6,5)
